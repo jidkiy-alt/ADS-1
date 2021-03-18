975c60cdcca9a73cdaf3d881bce532437e3c0af8
jidkiy-alt <76746846+jidkiy-alt@users.noreply.github.com>
2021-03-18 19:05:12 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,40 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int leftS = 0, count = 0;
+	int rightS = size-1;
+	bool midCenter = true;
+	int mid = (leftS + rightS) / 2;
+	while (leftS <= rightS)
+	{
+		if (midCenter)
+			mid = (leftS + rightS) / 2;
+		if (arr[mid] == value)
+		{
+			midCenter = false;
+			if ((arr[mid-1]) != value) 
+			{
+				leftS = mid;
+				while (arr[mid] == value)
+				{
+					count++;
+					mid++;
+					if (arr[mid] != value)
+						return count;
+				}
+			}
+			else 
+			{
+				while (arr[mid-1] == value)
+				{
+					mid--;
+				}
+			}
+		}
+		else if (arr[mid] > value)
+			rightS = mid-1;
+		else if (arr[mid] < value)
+			leftS = mid+1;	
+	}
+  	return 0;
 }
