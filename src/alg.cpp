819457a4e07e966a4542d3bb8ea4c3123abe2997
jidkiy-alt <76746846+jidkiy-alt@users.noreply.github.com>
2021-03-18 19:26:40 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,39 +2,32 @@
 
 int cbinsearch(int *arr, int size, int value) {
   int leftS = 0, count = 0;
-	int rightS = size-1;
-	bool midCenter = true;
-	int mid = (leftS + rightS) / 2;
-	while (leftS <= rightS)
-	{
-		if (midCenter)
-			mid = (leftS + rightS) / 2;
-		if (arr[mid] == value)
-		{
-			midCenter = false;
-			if ((arr[mid-1]) != value) 
-			{
-				leftS = mid;
-				while (arr[mid] == value)
-				{
-					count++;
-					mid++;
-					if (arr[mid] != value)
-						return count;
-				}
-			}
-			else 
-			{
-				while (arr[mid-1] == value)
-				{
-					mid--;
-				}
-			}
-		}
-		else if (arr[mid] > value)
-			rightS = mid-1;
-		else if (arr[mid] < value)
-			leftS = mid+1;	
-	}
-  	return 0;
+  int rightS = size-1;
+  bool midCenter = true;
+  int mid = (leftS + rightS) / 2;
+  while (leftS <= rightS) {
+     if (midCenter)
+        mid = (leftS + rightS) / 2;
+     if (arr[mid] == value) {
+        midCenter = false;
+        if ((arr[mid-1]) != value) {
+           leftS = mid;
+           while (arr[mid] == value) {
+              count++;
+              mid++;
+              if (arr[mid] != value)
+                 return count;
+           }
+        } else {
+             while (arr[mid-1] == value) {
+                mid--;
+             }
+          }
+      }
+      else if (arr[mid] > value)
+         rightS = mid-1;
+      else if (arr[mid] < value)
+         leftS = mid+1;
+   }
+   return 0;
 }
